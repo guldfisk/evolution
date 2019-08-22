c99b9877519f0dd706518c87305f67f1e09ad347
guldfisk <ce.guldfisk@gmail.com>
2019-08-22 16:48:06 +0200

    ok

--- evolution/model.py
+++ evolution/model.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import typing as t
 from abc import ABC, abstractmethod
+from collections import OrderedDict
 from copy import copy
 
 import functools
@@ -9,73 +10,6 @@ import operator
 import random
 
 import numpy as np
-
-# class Constraint(ABC):
-#     description = 'A constraint' #type: str
-# 
-#     def __init__(
-#         self,
-#     ):
-#         pass
-# 
-#     @abstractmethod
-#     def score(self, individual: 'Individual') -> float:
-#         pass
-# 
-# 
-# class ConstraintSet(object):
-# 
-#     def __init__(self, constraints: t.Tuple[t.Tuple[Constraint, float], ...]):
-#         self._constraints = constraints
-#         self._weights = tuple(weight for _, weight in self._constraints)
-# 
-#     def score(self, individual: 'Individual') -> t.Tuple[float, ...]:
-#         unweighted_values = tuple(
-#             constraint.score(individual)
-#             for constraint, _ in
-#             self._constraints
-#         )
-# 
-#         return (
-#                    functools.reduce(
-#                        operator.mul,
-#                        (
-#                            value ** weight
-#                            for value, weight in
-#                            zip(unweighted_values, self._weights)
-#                        )
-#                    ),
-#                ) + unweighted_values
-# 
-#     def total_score(self, individual: 'Individual') -> float:
-#         return self.score(individual)[0]
-# 
-#     def __iter__(self) -> t.Iterable[Constraint]:
-#         return (constraint for constraint, _ in self._constraints)
-# 
-# 
-# class ConstraintSetBluePrint(object):
-# 
-#     def __init__(self, *constraints: t.Tuple[t.Type[Constraint], float, t.Dict]):
-#         self._constraints = constraints
-# 
-#     def realise(
-#         self,
-#         # constrained_nodes: t.FrozenSet[ConstrainedNode],
-#         # trap_amount: int,
-#         # random_population: t.Collection[TrapDistribution],
-#     ) -> ConstraintSet:
-#         pass
-#         # return ConstraintSet(
-#         #     tuple(
-#         #         (
-#         #             constraint_type(constrained_nodes, trap_amount, random_population, **kwargs),
-#         #             weight,
-#         #         )
-#         #         for constraint_type, weight, kwargs in
-#         #         self._constraints
-#         #     )
-#         # )
 
 
 class Individual(ABC):
@@ -95,26 +29,98 @@ class Individual(ABC):
         pass
 
 
-# class Population(ABC):
-# 
-#     def __init__(self, individuals: t.List[Individual]):
-#         self._individuals = individuals
-# 
-#     # @abstractmethod
-#     # def serialize(self) -> t.AnyStr:
-#     #     pass
-#     #
-#     # @abstractmethod
-#     # def deserialize(self, s: t.AnyStr) -> 'Population':
-#     #     pass
+Generation = t.List[Individual]
+
+
+class LoggingOperation(ABC):
+
+    @abstractmethod
+    def inspect(self, values: t.List[t.Any]) -> t.Any:
+        pass
+
+
+class IndividualLoggingOperation(LoggingOperation):
+
+    @abstractmethod
+    def inspect(self, generation: Generation) -> t.Any:
+        pass
+
+
+class FitnessLoggingOperation(LoggingOperation):
+
+    @abstractmethod
+    def inspect(self, fitnesses: np.ndarray) -> t.Any:
+        pass
+
+
+class LogAverage(FitnessLoggingOperation):
+
+    def inspect(self, fitnesses: np.ndarray) -> t.Any:
+        return np.mean(fitnesses)
+
+
+class LogMedian(FitnessLoggingOperation):
+
+    def inspect(self, fitnesses: np.ndarray) -> t.Any:
+        return np.median(fitnesses)
+
+
+class LogMax(FitnessLoggingOperation):
+
+    def inspect(self, fitnesses: np.ndarray) -> t.Any:
+        return np.max(fitnesses)
+
+
+class Logger(object):
+
+    def __init__(self, operations: t.Optional[OrderedDict[str, LoggingOperation]] = None):
+        self._operations = operations if operations is not None else OrderedDict()
+        self._cache_fitnesses = any(
+            isinstance(operation, FitnessLoggingOperation)
+            for operation in
+            self._operations.values()
+        )
+
+        self._frames: t.List[OrderedDict[str, t.Any]] = []
+
+    def get_log_frame(self, generation: Generation) -> OrderedDict[str, t.Any]:
+        fitnesses = (
+            np.asarray((individual.fitness for individual in generation))
+            if self._cache_fitnesses else
+            None
+        )
+        frame = OrderedDict(
+            [
+                (
+                    key,
+                    operation.inspect(
+                        fitnesses
+                        if isinstance(operation, FitnessLoggingOperation) else
+                        generation
+                    ),
+                )
+                for key, operation in
+                self._operations.items()
+            ]
+        )
+
+        self._frames.append(frame)
+        return frame
 
 
 class Environment(ABC):
 
-    def __init__(self, individual_factory: t.Callable[[], Individual], initial_population_size: int):
+    def __init__(
+        self,
+        individual_factory: t.Callable[[], Individual],
+        initial_population_size: int,
+        mutate: t.Callable[[Individual], Individual],
+        mate: t.Callable[[Individual, Individual], t.Tuple[Individual, Individual]],
+        logger: t.Optional[Logger] = None,
+    ):
         self._individual_factory = individual_factory
         self._initial_population_size = initial_population_size
-        self._generations = [
+        self._generations: t.List[Generation] = [
             [
                 individual_factory()
                 for _ in
@@ -122,26 +128,23 @@ class Environment(ABC):
             ]
         ]
 
+        self._mutate = mutate
+        self._mate = mate
+
+        self._logger = logger if logger is not None else Logger()
+
         self._mutate_threshold = .25
         self._mate_threshold = .2
         self._tournament_size = 4
 
-    @abstractmethod
-    def mutate(self, individual) -> Individual:
-        pass
-
-    @abstractmethod
-    def mate(self, first_individual, second_individual) -> t.Tuple[Individual, Individual]:
-        pass
-
     @classmethod
     def print(cls, *args, **kwargs):
-        print(*args, sep='\t\t', **kwargs)
+        print(*(str(arg).ljust(50) for arg in args), **kwargs)
 
     def mutate_population(self) -> None:
         for individual in self._generations[-1]:
             if random.random() < self._mutate_threshold:
-                self.mutate(individual)
+                self._mutate(individual)
 
     def mate_population(self) -> None:
         for first, second in zip(
@@ -149,35 +152,37 @@ class Environment(ABC):
             self._generations[-1][1::2],
         ):
             if random.random() < self._mate_threshold:
-                self.mate(first, second)
+                self._mate(first, second)
 
-    def next_generation(self) -> Environment:
+    def spawn_generation(self) -> Environment:
+        new_generation = [
+            copy(
+                sorted(
+                    random.sample(
+                        self._generations[-1],
+                        self._tournament_size,
+                    ),
+                    key=lambda individual: individual.fitness,
+                )[-1]
+            )
+            for _ in
+            range(self._initial_population_size)
+        ]
         self._generations.append(
-            [
-                copy(
-                    sorted(
-                        random.sample(
-                            self._generations[-1],
-                            self._tournament_size,
-                        ),
-                        key=lambda individual: individual.fitness,
-                    )[-1]
-                )
-                for _ in
-                range(self._initial_population_size)
-            ]
+            new_generation
         )
         
         self.mate_population()
-        self.mate_population()
+        self.mutate_population()
 
-        self.print(len(self._generations) - 1, self.mean(), self.fittest().fitness)
+        frame = self._logger.get_log_frame(new_generation)
+        self.print(frame.values())
 
         return self
     
-    def generations(self, amount: int) -> Environment:
+    def spawn_generations(self, amount: int) -> Environment:
         for _ in range(amount):
-            self.next_generation()
+            self.spawn_generation()
         return self
     
     def fittest(self) -> Individual:
@@ -185,12 +190,3 @@ class Environment(ABC):
             self._generations[-1],
             key = lambda individual: individual.fitness,
         )[-1]
-
-    def mean(self) -> float:
-        return np.mean(
-            [
-                individual.fitness
-                for individual in
-                self._generations[-1]
-            ]
-        )
