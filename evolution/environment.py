2a173cd11c9f0e9e4342c0328872b693517c64de
guldfisk <ce.guldfisk@gmail.com>
2019-08-29 16:05:03 +0200

    ok

---
+++ evolution/environment.py
@@ -0,0 +1,114 @@
+from __future__ import annotations
+
+import typing as t
+
+import random
+import copy
+
+from evolution.logging import Logger, LogFrame
+from evolution.model import Individual, ConstraintSet, Generation
+
+
+I = t.TypeVar('I', bound = Individual)
+
+
+class Environment(t.Generic[I]):
+
+    def __init__(
+        self,
+        individual_factory: t.Callable[[], I],
+        initial_population_size: int,
+        mutate: t.Callable[[I, Environment], I],
+        mate: t.Callable[[I, I, Environment], t.Tuple[I, I]],
+        constraints: ConstraintSet,
+        logger: t.Optional[Logger] = None,
+    ):
+        self._individual_factory = individual_factory
+        self._initial_population_size = initial_population_size
+        self._generations: t.List[Generation] = []
+
+        self._mutate = mutate
+        self._mate = mate
+
+        self._constraints: ConstraintSet = constraints
+
+        self._logger: Logger = logger if logger is not None else Logger()
+
+        self._mutate_threshold: float = .3
+        self._mate_threshold: float = .3
+        self._tournament_size: int = 4
+
+
+    def _get_initial_generation(self) -> Generation:
+        generation = [
+            self._individual_factory()
+            for _ in
+            range(self._initial_population_size)
+        ]
+        for individual in generation:
+            setattr(individual, '_fitness', self._constraints.score(individual))
+
+        return generation
+
+    def mutate_population(self, generation: Generation) -> None:
+        for individual in generation:
+            if random.random() < self._mutate_threshold:
+                self._mutate(individual, self)
+                setattr(individual, '_changed', True)
+
+    def mate_population(self, generation: Generation) -> None:
+        for first, second in zip(
+            generation[0::2],
+            generation[1::2],
+        ):
+            if random.random() < self._mate_threshold:
+                self._mate(first, second, self)
+                setattr(first, '_changed', True)
+                setattr(second, '_changed', True)
+
+    def spawn_generation(self) -> LogFrame:
+        if not self._generations:
+            new_generation = self._get_initial_generation()
+
+        else:
+            new_generation = [
+                copy.deepcopy(
+                    sorted(
+                        random.sample(
+                            self._generations[-1],
+                            self._tournament_size,
+                        ),
+                        key = lambda _individual: _individual.fitness[0],
+                    )[-1]
+                )
+                for _ in
+                range(self._initial_population_size)
+            ]
+
+            for individual in new_generation:
+                if hasattr(individual, '_changed'):
+                    delattr(individual, '_changed')
+
+            self.mate_population(new_generation)
+            self.mutate_population(new_generation)
+
+            for individual in new_generation:
+                if hasattr(individual, '_changed'):
+                    setattr(individual, '_fitness', self._constraints.score(individual))
+
+        self._generations.append(
+            new_generation
+        )
+
+        return self._logger.add_frame(new_generation)
+
+    def spawn_generations(self, amount: int) -> Environment:
+        for _ in range(amount):
+            self.spawn_generation()
+        return self
+
+    def fittest(self) -> I:
+        return sorted(
+            self._generations[-1],
+            key = lambda individual: individual.fitness,
+        )[-1]
---
+++ evolution/logging.py
@@ -0,0 +1,105 @@
+from __future__ import annotations
+
+import typing as t
+
+from abc import ABC, abstractmethod
+from collections import OrderedDict
+
+import numpy as np
+
+from evolution.model import Generation
+
+
+LogFrame = t.List[t.Any]
+
+
+class LoggingOperation(ABC):
+
+    @abstractmethod
+    def inspect(self, values: LogFrame) -> t.Any:
+        pass
+
+
+class IndividualLoggingOperation(LoggingOperation):
+
+    @abstractmethod
+    def inspect(self, generation: Generation) -> t.Any:
+        pass
+
+
+class FitnessLoggingOperation(LoggingOperation):
+
+    @abstractmethod
+    def inspect(self, fitnesses: np.ndarray) -> t.Any:
+        pass
+
+
+class LogAverage(FitnessLoggingOperation):
+
+    def inspect(self, fitnesses: np.ndarray) -> t.Any:
+        return np.mean(fitnesses)
+
+
+class LogMedian(FitnessLoggingOperation):
+
+    def inspect(self, fitnesses: np.ndarray) -> t.Any:
+        return np.median(fitnesses)
+
+
+class LogMax(FitnessLoggingOperation):
+
+    def inspect(self, fitnesses: np.ndarray) -> t.Any:
+        return np.max(fitnesses)
+
+
+class Logger(object):
+
+    def __init__(self, operations: t.Optional[OrderedDict[str, LoggingOperation]] = None):
+        self._operations = operations if operations is not None else OrderedDict()
+
+        self._cache_fitnesses = any(
+            isinstance(operation, FitnessLoggingOperation)
+            for operation in
+            self._operations.values()
+        )
+
+        self._values: t.List[LogFrame] = []
+
+    @property
+    def operations(self) -> t.Mapping[str, LoggingOperation]:
+        return self._operations
+
+    @property
+    def labels(self) -> t.Iterable[str]:
+        return self._operations.keys()
+
+    @property
+    def values(self) -> t.Sequence[t.Sequence[t.Any]]:
+        return self._values
+
+    def _get_frame(self, generation: Generation) -> LogFrame:
+        fitnesses = (
+            np.asarray([individual.fitness[0] for individual in generation])
+            if self._cache_fitnesses else
+            None
+        )
+        return [
+            operation.inspect(
+                fitnesses
+                if isinstance(operation, FitnessLoggingOperation) else
+                generation
+            )
+            for operation in
+            self._operations.values()
+        ]
+
+    def add_frame(self, generation: Generation) -> LogFrame:
+        frame = self._get_frame(generation)
+        self._values.append(
+            frame
+        )
+        return frame
+    
+    
+def print_log_frame(frame: LogFrame):
+    print(*(str(arg).ljust(35) for arg in frame))
